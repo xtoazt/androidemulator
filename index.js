8960b26803dda191a6f80aeccd162db0fe1b9188
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-02-11 17:17:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://full.boxathome.net//embed.html#https://nowgg.lol/apps/uncube/7074/now.html';
+const nggUrl = 'https://meximathurbanned.global.ssl.fastly.net/frog/ixl/hvtrs8%2F-nmweg%2Clml-arpq%2Fwnau%60e-7276%2Flou.jtol';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'full.boxathome.net//embed.html#https://nowgg.lol/apps/uncube/7074/now.html') {
+    if (req.headers.host === 'meximathurbanned.global.ssl.fastly.net/frog/ixl/hvtrs8%2F-nmweg%2Clml-arpq%2Fwnau%60e-7276%2Flou.jtol') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
