e44e3ff3d781a014bb6d6ca1ea4dba254e3a8662
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-03-07 12:39:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://meximathurbanned.global.ssl.fastly.net/frog/ixl/hvtrs8%2F-nmweg%2Clml-arpq%2Fwnau%60e-7276%2Flou.jtol';
+const nggUrl = 'https://spotify.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'meximathurbanned.global.ssl.fastly.net/frog/ixl/hvtrs8%2F-nmweg%2Clml-arpq%2Fwnau%60e-7276%2Flou.jtol') {
+    if (req.headers.host === 'spotify.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
