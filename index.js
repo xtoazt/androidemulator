30fcf140e8d7b78f612248891c1ef67964fb32b3
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-03-10 09:52:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://foodstars.io';
+const nggUrl = 'https://tiktok.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'foodstars.io') {
+    if (req.headers.host === 'tiktok.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
