e96ea2ca563dc1cc3ee56f83a240d32bb8969901
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-03-10 13:28:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://tiktok.com';
+const nggUrl = 'https://firemovies.vercel.app';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'tiktok.com') {
+    if (req.headers.host === 'firemovies.vercel.app') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
